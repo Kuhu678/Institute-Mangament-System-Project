750e3c79600e40ec45ef728a42f5afa2b71f9023
kuhu kansal <kuhu.kansalbsr@gmail.com>
2022-08-30 12:20:56 +0530

    projectt

---
+++ UnitTestProject1/ControllerTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SMS.Controllers;
+using System;
+using System.Web.Mvc;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ControllerTests
+    {
+        [TestMethod]
+        public void Index()
+        {
+
+        //Arrange
+
+            HomeController controller = new HomeController();
+
+
+        //Act
+        ViewResult result = controller.Index()   as ViewResult;
+
+        //Assert
+        Assert.IsNotNull(result);
+
+        }
+
+        public void About()
+        {
+
+            //Arrange
+
+            HomeController controller = new HomeController();
+
+
+            //Act
+            ViewResult result = controller.About() as ViewResult;
+
+            //Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+
+        }
+
+        public void Contact()
+        {
+
+            //Arrange
+
+            HomeController controller = new HomeController();
+
+
+            //Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            //Assert
+            Assert.AreEqual("Your contact page..", result.ViewBag.Message);
+
+        }
+
+    }
+}
---
+++ studentsmanagement/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("studentsmanagement")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("studentsmanagement")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fd74a74b-82d6-4a64-85cd-7bfc931ccde2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
